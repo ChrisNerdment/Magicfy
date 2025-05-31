f92fb00d3a5c326eebba6cb7446c77e1be5c3c06
Chris Nerdment <chris@nerdment.games>
2025-05-31 14:27:37 +0200

    Update homepage message to reflect version 2 of Magicfy.Web

--- Magicfy.Web/Program.cs
+++ Magicfy.Web/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from Magicfy.Web!");
+app.MapGet("/", () => "Hello World from Magicfy.Web Version 2!");
 
 app.MapGet("/api/hello", () => new { Message = "Hello World!", Application = "Magicfy.Web", Timestamp = DateTime.Now });
 
