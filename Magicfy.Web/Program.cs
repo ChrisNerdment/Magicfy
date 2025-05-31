0ca52b8888864c45c72fd209d33baa3bfac31880
Chris Nerdment <chris@nerdment.games>
2025-05-31 11:29:42 +0200

    Update greeting message and add API endpoint for hello response

--- Magicfy.Web/Program.cs
+++ Magicfy.Web/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World from Magicfy.Web!");
+
+app.MapGet("/api/hello", () => new { Message = "Hello World!", Application = "Magicfy.Web", Timestamp = DateTime.Now });
 
 app.Run();
