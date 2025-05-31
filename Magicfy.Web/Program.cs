be509f10c65a745276d7f44ef8764e23b720bb9f
Chris Nerdment <chris@nerdment.games>
2025-05-31 14:39:30 +0200

    Enhance root endpoint to include debug and console logging for version 2

--- Magicfy.Web/Program.cs
+++ Magicfy.Web/Program.cs
@@ -1,7 +1,15 @@
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World from Magicfy.Web Version 2!");
+app.MapGet("/", () =>
+{
+    Debug.WriteLine("Hello World from Magicfy.Web Version 2!");
+    Console.WriteLine("Hello World Console from Magicfy.Web Version 2!");
+    return "Hello World from Magicfy.Web Version 2!";
+});
 
 app.MapGet("/api/hello", () => new { Message = "Hello World!", Application = "Magicfy.Web", Timestamp = DateTime.Now });
 
